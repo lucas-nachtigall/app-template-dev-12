dc7823b6353b2cf333d8d755640ea4318d21d551
Lucas Garcia Nachtigall <69644385+lucas-nachtigall@users.noreply.github.com>
2023-01-09 22:11:01 -0300

    logs

--- api/core/index.js
+++ api/core/index.js
@@ -1,4 +1,6 @@
 exports.handler = async (event) => {
+    console.log("aaaa");
+    console.error("bbbb");
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello World!!'),
